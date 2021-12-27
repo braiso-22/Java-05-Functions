8a608b8a7d00695f8e3055e74ffc758056acea53
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-27 14:42:55 +0100

    Ej02 terminado

---
+++ Java-05-Functions/src/functions/Ej02.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package functions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brais
+ */
+public class Ej02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static Scanner a = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        int num = -1;
+        while (num != 0) {
+            System.out.println("------------Menú------------");
+            System.out.println("-  1. Area del círculo     -");
+            System.out.println("-  2. Area del cuadrado    -");
+            System.out.println("-  3. Area del triangulo   -");
+            System.out.println("-  0. Salir                -");
+            System.out.println("----------------------------");
+            num = a.nextInt();
+            switch (num) {
+                case 1:
+                    System.out.println("Introduce el radio del círculo: ");
+                    System.out.println("El area es: " + circulo(a.nextFloat()));
+                    break;
+                case 2:
+                    System.out.println("Introduce el lado del cuadrado: ");
+                    System.out.println("El area es: " + cuadrado(a.nextFloat()));
+                    break;
+                case 3:
+                    System.out.println("Introduce la base y altura del triangulo: ");
+                    System.out.println("El area es: " + triangulo(a.nextFloat(), a.nextFloat()));
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no válida");
+                    break;
+
+            }
+        }
+
+    }
+
+    public static double circulo(float radio) {
+        double area;
+        area = Math.PI * Math.pow(radio, 2);
+        return area;
+    }
+
+    public static double cuadrado(float l) {
+        double area;
+        area = Math.pow(l, 2);
+        return area;
+    }
+
+    public static double triangulo(float b, float h) {
+        double area;
+        area = b * h / 2;
+        return area;
+    }
+
+}
